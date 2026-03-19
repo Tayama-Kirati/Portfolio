47bec66a8cbc6357a9efe44c71a72c852d361c77
Tayama Kirati <tayamab2003@gmail.com>
2026-03-19 22:22:33 +0545

    frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,28 @@
+  
+import './App.css'
+import { RouterProvider } from "react-router-dom"
+import router from "./routes"
  
+import Navbar from './globals/navbar/Navbar'
+import Footer from './globals/footer/Footer'
+import Home from './pages/home/Home'
+import Product from './pages/home/components/Product'
+ 
+ 
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    
+    <RouterProvider router = {router}/>
+   
+    <Footer/>
+     
+
+     </>
+  )
+}
+
+export default App
